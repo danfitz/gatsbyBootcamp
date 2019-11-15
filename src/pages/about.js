27f0c447d52d04ceefb0fed361adfad0223a399c
Dan Fitz <dan@danfitz.com>
2019-11-15 15:06:21 -0600

    Fixed link to twitter profile

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 import classes from "./about.css";
 import Layout from "../components/Layout"; 
 
@@ -7,7 +6,7 @@ const AboutPage = () => {
   return (
     <Layout title="About">
       <h1>About Me</h1>
-      <p>Twitter: <Link to="https://twitter.com/_danfitz">@_danfitz</Link></p>
+      <p>Twitter: <a href="https://twitter.com/_danfitz">@_danfitz</a></p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptates, sint eaque adipisci architecto doloremque. Corrupti, fuga amet excepturi debitis minus exercitationem porro qui nobis rem, tempora sed mollitia autem expedita aperiam fugit magnam possimus natus officia. Eos sint optio consectetur. Libero, delectus, sit porro officia dicta debitis culpa veniam quisquam deserunt illo rerum sunt voluptatibus enim quaerat ad optio voluptas id placeat consectetur, beatae illum quia nobis eius! Quibusdam nemo rem vel aperiam. Accusantium eos architecto laudantium quasi nemo adipisci distinctio asperiores rerum corrupti at expedita repellat, impedit odit voluptates. Placeat fugit, sapiente autem optio dignissimos quisquam. Minus, modi!</p>
     </Layout>
   );
